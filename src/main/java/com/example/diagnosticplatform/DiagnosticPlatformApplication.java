7cb090b9a50b8acb5799993f8ff95d68d8949da4
Sara Oueld Beyad <sarao.beyad@gmail.com>
2023-10-04 11:31:17 +0100

    Initial commit

---
+++ src/main/java/com/example/diagnosticplatform/DiagnosticPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.diagnosticplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagnosticPlatformApplication.class, args);
+    }
+
+}
